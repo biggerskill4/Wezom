aeff11348bb6b5b2e79f37e192103635af469d85
John Doe <johndoe@email.com>
2024-03-17 04:37:18 +0500

    resposive

---
+++ Assets/JS/main.js
@@ -0,0 +1,8 @@
+//side nav
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
