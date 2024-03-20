2865d37d3da72cc1096dcb79d177f5fa1a91fb66
John Doe <johndoe@email.com>
2024-03-20 20:16:53 +0500

    mega menu

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -6,16 +6,26 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   };
 
+// auto text change
+// banner h3 (heading)
   (function () {
-    var title = [
-      "250+",
-      "3500+",
-      "250+"
-    ],
+    var title = ["23", "250+","3500+", ],
     i = 0;
-    setInterval(function(){
-      $('#h3').fadeOut(function(){
-        $(this).html(words[(i = (i = 1) % title.length)]).fadeIn();
-      });
-    }, 1000)
+    setInterval(function(){ $('#h3').fadeOut(function(){
+        $(this).html(title[(i = (i + 1) % title.length)]).fadeIn();
+      }); }, 3000)
   })();
+// text
+  (function () {
+    var words = ["years of experience", "full-time professionals on staff","Implemented projects in IT", ],
+    i = 0;
+    setInterval(function(){ $('#words').fadeOut(function(){
+        $(this).html(words[(i = (i + 1) % words.length)]).fadeIn();
+      }); }, 3000)
+  })();
+// arrows
+  (function () {
+    setInterval(function() {
+        $('#arrow').fadeOut().delay(50).fadeIn();
+    }, 3000);
+})();
